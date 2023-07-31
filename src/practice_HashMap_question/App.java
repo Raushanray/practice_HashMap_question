b9cfacc1ea71d5eedff1637c45cf841479c8a64d
Raushanray <raushankuray1998@gmail.com>
2023-07-31 17:14:59 +0530

    create a java project and here add a hashmap with the immutable key and value

---
+++ src/practice_HashMap_question/App.java
@@ -0,0 +1,17 @@
+package practice_HashMap_question;
+
+import java.util.HashMap;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		  HashMap<String, String> map = new HashMap<>(); map.put("Raushan", "India");
+		  
+		  System.out.println(map.get("Raushan"));
+		 
+
+		
+		
+	}
+}
