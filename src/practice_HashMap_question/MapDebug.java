6b025f3e6eb994b6e3af68279d5628c486fbf2a4
Raushanray <raushankuray1998@gmail.com>
2023-08-09 16:03:36 +0530

    hashcode collision..

--- src/practice_HashMap_question/MapDebug.java
+++ src/practice_HashMap_question/MapDebug.java
@@ -8,12 +8,16 @@ import java.util.Map;
 
 public class MapDebug {
 	public static void main(String[] args) {
+		/*
+		 * hashcode collision because both s1 and s2 have same hashcode..
+		 */
+		String s1 = "FB";
+		String s2 = "Ea";
+		System.out.println(s1.hashCode() + " " + s2.hashCode());
 		
 		Map<String, Integer> map = new HashMap<>();
-		map.put("aaa", 1);
-		map.put("bbb", 2);
-		map.put("ccc", 3);
-		map.put("ddd", 4);
+		map.put(s1, 1);
+		map.put(s2, 2);
 	}
 
 }
