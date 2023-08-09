a445d240439a017662d676b17a4dd533f146265c
Raushanray <raushankuray1998@gmail.com>
2023-08-09 16:21:57 +0530

    HashMap internals During Duplicate Key..

--- src/practice_HashMap_question/MapDebug.java
+++ src/practice_HashMap_question/MapDebug.java
@@ -11,13 +11,19 @@ public class MapDebug {
 		/*
 		 * hashcode collision because both s1 and s2 have same hashcode..
 		 */
-		String s1 = "FB";
-		String s2 = "Ea";
-		System.out.println(s1.hashCode() + " " + s2.hashCode());
+		/*
+		 * String s1 = "FB"; String s2 = "Ea"; System.out.println(s1.hashCode() + " " +
+		 * s2.hashCode());
+		 */
 		
 		Map<String, Integer> map = new HashMap<>();
-		map.put(s1, 1);
-		map.put(s2, 2);
+		//HashMap Duplicate Key
+		/*
+		 * here i have both put have same key so here when i debug this then first get the value 1 and
+		 *  when i go with second then the value = 2 simply update the value...because the hashcode is same...
+		 */
+		map.put("aa", 1);
+		map.put("aa", 2);
 	}
 
 }
