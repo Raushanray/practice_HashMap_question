82c33766bff2391bbef34d645a4d95002549dbf3
Raushanray <raushankuray1998@gmail.com>
2023-08-09 15:56:53 +0530

    Hash map internally wroking...

---
+++ src/practice_HashMap_question/MapDebug.java
@@ -0,0 +1,19 @@
+package practice_HashMap_question;
+
+import java.util.HashMap;
+import java.util.Map;
+/*
+ * HashMap internally work.
+ */
+
+public class MapDebug {
+	public static void main(String[] args) {
+		
+		Map<String, Integer> map = new HashMap<>();
+		map.put("aaa", 1);
+		map.put("bbb", 2);
+		map.put("ccc", 3);
+		map.put("ddd", 4);
+	}
+
+}
