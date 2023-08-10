3a279d41941f2bc12a9af1ca6de2e85244132b12
Raushanray <raushankuray1998@gmail.com>
2023-08-10 15:55:56 +0530

    now create a new string and find the duplicate character of this string....

--- src/practice_HashMap_question/DuplicateString.java
+++ src/practice_HashMap_question/DuplicateString.java
@@ -35,12 +35,35 @@ public class DuplicateString {
 				System.out.println("The Word \""+ temp + "\" appred "+ hm.get(temp) + " no of times" );
 			}
 		 }
+		 System.out.println("============================================================");
 	}
 	
+	 
+	  static void findDuplicateCharacters(String str) {
+			HashMap<Character, Integer> hm = new HashMap<>();
+			for (int i = 0; i < str.length(); i++) {
+				char c = str.charAt(i);
+				if (hm.get(c) != null) {
+					hm.put(c, hm.get(c)+1);
+				}else
+				{
+					hm.put(c, 1);
+				}
+			}
+//			System.out.println(hm);
+			 Iterator<Character> charString = hm.keySet().iterator();
+			 while(charString.hasNext())
+			 {
+				 Character temp = charString.next();
+				 if (hm.get(temp) > 1) {
+					System.out.println("The Character \""+ temp + "\" appred "+ hm.get(temp) + " no of times" );
+				}
+			 }
+		}
+	 
 	public static void main(String[] args) {
 		findDuplicateWord("i am am learning java java");
+		findDuplicateCharacters("javaj2ee");
 	}
-
-	
 
 }
