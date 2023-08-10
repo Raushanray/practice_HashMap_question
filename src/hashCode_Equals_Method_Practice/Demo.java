54d3b0e3fde43857567d14b475f8c843203ef5bc
Raushanray <raushankuray1998@gmail.com>
2023-08-10 19:43:44 +0530

    for this above problem here i am implementing..the hashcode and equals methods in employee class...

--- src/hashCode_Equals_Method_Practice/Demo.java
+++ src/hashCode_Equals_Method_Practice/Demo.java
@@ -37,6 +37,31 @@ class Employee{
 	public Employee(int id) {
 		this.id = id;
 	}
+	/*
+	 * for this above problem here i am implementing..the hashcode and equals methods 
+	 */
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
 	
 	
 }
