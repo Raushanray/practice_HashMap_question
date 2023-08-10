d124bbf910876e9813d2be912bbc5e1aed5e14bf
Raushanray <raushankuray1998@gmail.com>
2023-08-10 15:05:20 +0530

    now i am creating hashmap and here put the loop value(tempString) and count as 1 of every word in String.. and then print them..

--- src/practice_HashMap_question/DuplicateString.java
+++ src/practice_HashMap_question/DuplicateString.java
@@ -12,12 +12,16 @@ public class DuplicateString {
 	
 	 static void findDuplicateWord(String str) {
 
+		 HashMap<String, Integer> hm = new HashMap<>();
 		 String[] s = str.split(" ");
 		 for (String tempString: s) {
 			 //just for debugging.
-			System.out.println(tempString);
+//			System.out.println(tempString);
 			 
-		 }
+			hm.put(tempString, 1);
+		 
+	}
+	 System.out.println(hm);
 	}
 	
 	public static void main(String[] args) {
